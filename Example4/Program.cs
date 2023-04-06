91c0da50de4119b33b30acc2c2b01c71e0650a8f
Nikita <crazefire@mail.ru>
2023-04-07 00:44:04 +0300

    Выполнено 4 задание

---
+++ Example4/Program.cs
@@ -0,0 +1,9 @@
+﻿double numberA = new Random().Next(1, 10); //1 2 3 ... 9
+double numberB = new Random().Next(1, 10); //1 2 3 ... 9
+double sum = numberA + numberB;
+Console.Write("Первое число - ");
+Console.WriteLine(numberA);
+Console.Write("Второе число - ");
+Console.WriteLine(numberB);
+Console.Write("Сумма - ");
+Console.WriteLine(sum);
